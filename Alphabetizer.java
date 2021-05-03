cf1f0e0ab0361ba0170f46c174d8045ddfc468cc
Utsav Vohra <utsav2385@gmail.com>
2021-05-03 15:17:23 -0400

    adding file

---
+++ Alphabetizer.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Alphabetizer {
+
+	
+	
+	public String operation(String str)
+	{
+		str = str.replaceAll(
+		          "[^a-zA-Z]", "");
+		Character tempArray[] = new Character[str.length()];
+        
+     System.out.println("str="+str);
+      
+     
+    
+		for (int i = 0; i < str.length(); i++) {
+			tempArray[i] = str.charAt(i);
+		}
+		
+		
+	
+		Arrays.sort(tempArray, new Comparator<Character>(){
+
+			@Override
+			public int compare(Character c1, Character c2)
+			{
+			
+				return Character.compare(Character.toLowerCase(c1),
+										Character.toLowerCase(c2));
+			}
+		});
+		
+		
+		StringBuilder sb = new StringBuilder(tempArray.length);
+		for (Character c : tempArray)
+			sb.append(c.charValue());
+
+		return sb.toString();
+	}
+	
+	public static void main(String args[])
+	{
+		Alphabetizer p=new Alphabetizer();
+	
+		System.out.println("Result="+p.operation("3 Blind Mice"));
+	}
+	
+}
